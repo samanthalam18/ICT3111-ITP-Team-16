4b8d88af893189851d18f2686eea560fba3f74a4
Ye Kai Phang <38521895+yekai11@users.noreply.github.com>
2023-08-02 11:39:00 +0800

    cleanup

--- KAIR/gitscript.sh
+++
@@ -1,5 +0,0 @@
-git branch | while read branch; do
-    git checkout $branch
-    git filter-branch --tree-filter 'find . -name "*.jpg" -o -name "*.png" -o -name "*.pth" -exec rm -f {} \;' HEAD
-    git reflog expire --expire=now --all && git gc --prune=now --aggressive
-done
